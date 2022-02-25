ab17c7da9f7caa563d78b0eaf85db2506fdafd6c
Onur <the_old_republic@windowslive.com>
2022-02-25 17:43:07 -0500

    Trying to change it

--- Scripts/app.js
+++ Scripts/app.js
@@ -95,7 +95,7 @@
         console.log("Home Page");
         $("#AboutUsButton").on("click", () => 
         {
-            location.href = "/about";
+            location.href = "#/about";
         });
     
         $("main").append(`<p id="MainParagraph" class="mt-3">This is the Main Paragraph</p>`);
@@ -236,18 +236,18 @@
                 {
                     localStorage.removeItem($(this).val())
                 }
-                location.href = "/contact-list";
+                location.href = "#/contact-list";
             });
 
             $("button.edit").on("click", function()
             {
-                location.href = "/edit#" + $(this).val();
+                location.href = "#/edit#" + $(this).val();
             });
         }
 
         $("#addButton").on("click", ()=>
         {
-            location.href = "/edit#add";
+            location.href = "#/edit#add";
         });
     }
 
@@ -274,12 +274,12 @@
                     {
                         event.preventDefault();
                         AddContact(fullName.value, contactNumber.value, emailAddress.value);
-                        location.href = "/contact-list";
+                        location.href = "#/contact-list";
                     });
 
                     $("#cancelButton").on("click", () =>
                     {
-                        location.href = "/contact-list";
+                        location.href = "#/contact-list";
                     });
                 }
                 break;
@@ -306,12 +306,12 @@
                         // replace the item in local storage
                         localStorage.setItem(page, contact.serialize());
                         // go back to the contact list page (refresh)
-                        location.href = "/contact-list";
+                        location.href = "#/contact-list";
                     });
 
                     $("#cancelButton").on("click", () =>
                     {
-                        location.href = "/contact-list";
+                        location.href = "#/contact-list";
                     });
                     
                 }
@@ -357,7 +357,7 @@
                     messageArea.removeAttr("class").hide();
 
                     // redirect the user to the secure area of our site - contact-list.html
-                    location.href = "/contact-list";
+                    location.href = "#/contact-list";
                 }
                 // else if bad credentials were entered...
                 else
@@ -375,7 +375,7 @@
             document.forms[0].reset();
 
             // return to the home page
-            location.href = "/home";
+            location.href = "#/home";
         });
     }
 
@@ -395,7 +395,7 @@
                 sessionStorage.clear();
 
                 // redirect back to login
-                location.href = "/login";
+                location.href = "#/login";
             });
         }
     }
